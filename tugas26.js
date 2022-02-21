7b29e131eb082058158a2de60f8c6f54bd509077
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 14:53:25 +0700

    Add files via upload

---
+++ tugas26.js
@@ -0,0 +1,91 @@
+if (isFinite(2)) {
+  {
+    console.log("Angka 2 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 2 is Infinity");
+  }
+}
+
+if (isFinite(39)) {
+  {
+    console.log("Angka 39 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 39 is Infinity");
+  }
+}
+
+if (isFinite(76)) {
+  {
+    console.log("Angka 76 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 76 is Infinity");
+  }
+}
+if (isFinite(50)) {
+  {
+    console.log("Angka 50 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 50 is Infinity");
+  }
+}
+
+if (isFinite(9)) {
+  {
+    console.log("Angka 9 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 9 is Infinity");
+  }
+}
+if (isFinite(7)) {
+  {
+    console.log("Angka 7 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 7 is Infinity");
+  }
+}
+if (isFinite(41)) {
+  {
+    console.log("Angka 41 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 41 is Infinity");
+  }
+}
+if (isFinite(2)) {
+  {
+    console.log("Angka 2 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 2 is Infinity");
+  }
+}
+if (isFinite(24)) {
+  {
+    console.log("Angka 24 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 24 is Infinity");
+  }
+}
+if (isFinite(1)) {
+  {
+    console.log("Angka 1 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 1 is Infinity");
+  }
+}
+if (isFinite(16)) {
+  {
+    console.log("Angka 16 is NOT Infinity");
+  }
+  if (false) {
+    console.log("Angka 16 is Infinity");
+  }
+}
